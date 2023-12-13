cc0463ead1dcdad7420d15d4c0a279a0ab8e5a4d
CoolDawm <92115826+CoolDawm@users.noreply.github.com>
2023-12-13 21:52:30 +0300

    [Script addition][Environment]Terrain concept, disable\enable cursor on C button

--- ProjectS/Assets/Scripts/UI/UIManager.cs
+++ ProjectS/Assets/Scripts/UI/UIManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,7 @@ using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+    private bool cursorOn=false;
     void Start()
     {
         if (SceneManager.GetActiveScene().name != "CharacterSelection")
@@ -18,5 +20,23 @@ public class UIManager : MonoBehaviour
             Cursor.visible = true;
         }
        
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            if (cursorOn)
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+            cursorOn = !cursorOn;
+        }
     }
 }
